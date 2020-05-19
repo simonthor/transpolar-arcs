e63ae2bffd4f682f4c078bb8a217c52230f7ddba
Simon Thor <simon11.thor@hotmail.se>
2020-05-19 22:58:57 +0200

    Made test_OMNI.py ~30% faster. This code (specifically scipy.io.loadmat) is the bottleneck for the performance of the code.

--- test_OMNI.py
+++ test_OMNI.py
@@ -1,27 +1,9 @@
-
 import datetime
 import scipy.io
 import numpy
-import importlib
-test_OMNI = importlib.import_module('test_OMNI')
 
 
-if __name__ == "__main__":
-
-    data_dir = '/home/lcai/01_work/00_data/OMNI/OMNI_1min_Lv1/'
-    dt_start = datetime.datetime(2013, 11, 3, 0, 0, 0)
-    dt_stop = datetime.datetime(2013, 11, 5, 0, 0, 0)
-    paras_in = ['BxGSM', 'ByGSM', 'BzGSM', 'vel']
-
-    loadObj = test_OMNI.LoadOMNI(dt_start, dt_stop, data_dir=data_dir)
-    # loadObj.laod_OMNI_data() # load all the parameters
-    loadObj.load_OMNI_data(paras_in=paras_in)
-    BxGSM = loadObj.paras['BxGSM']
-    datetimelist = loadObj.paras['datetime']
-
-
-
-class LoadOMNI(object):
+class LoadOMNI:
     def __init__(self, dt_start, dt_stop, data_dir=None):
 
         diff_month = (dt_stop.year - dt_start.year) * 12 + dt_stop.month - dt_start.month
@@ -67,8 +49,8 @@ class LoadOMNI(object):
         para_dict = dict((paraname, ind) for ind, paraname in enumerate(full_para_list))
         if paras_in is None:
             paras_in = full_para_list
-        paras = dict((paraname, numpy.empty((0, 1))) for paraname in paras_in)
-        paras['datetime'] = numpy.empty((0, 1))
+        paras = dict((paraname, numpy.empty(0)) for paraname in paras_in)
+        paras['datetime'] = numpy.empty(0, dtype=numpy.datetime64)
         for ind_f, filename in enumerate(self.data_files):
             matfile = scipy.io.loadmat(self.data_dir + filename)
             edesh = matfile['edesh']
@@ -83,24 +65,30 @@ class LoadOMNI(object):
                 ind_t2 = edesh.shape[0]
             else:
                 ind_t2 = numpy.where(edesh <= dtdelta_stop)[0][-1]
-            ind_t = list(range(ind_t1, ind_t2))
 
-            dts = [dt0 + datetime.timedelta(seconds=second[0])
-                   for second in edesh[ind_t].tolist()]
+            dts = numpy.datetime64(dt0) + edesh[ind_t1:ind_t2].astype('timedelta64[s]')
 
-            dts = numpy.array(dts).reshape((len(ind_t), 1))
-            paras['datetime'] = numpy.vstack((paras['datetime'], dts))
+            paras['datetime'] = numpy.append(paras['datetime'], dts)
             data = matfile['comp']
             for paraname in paras_in:
                 if paraname == 'BxGSM':
-                    newdata = data[ind_t, para_dict['BxGSE']].reshape((len(ind_t), 1))
+                    newdata = data[ind_t1:ind_t2, para_dict['BxGSE']]
                 else:
-                    newdata = data[ind_t, para_dict[paraname]].reshape((len(ind_t), 1))
+                    newdata = data[ind_t1:ind_t2, para_dict[paraname]]
 
-                paras[paraname] = numpy.vstack((paras[paraname], newdata))
+                paras[paraname] = numpy.append(paras[paraname], newdata)
         self.paras = paras
 
 
+if __name__ == "__main__":
 
+    data_dir = '/home/lcai/01_work/00_data/OMNI/OMNI_1min_Lv1/'
+    dt_start = datetime.datetime(2013, 11, 3, 0, 0, 0)
+    dt_stop = datetime.datetime(2013, 11, 5, 0, 0, 0)
+    paras_in = ['BxGSM', 'ByGSM', 'BzGSM', 'vel']
 
-
+    loadObj = LoadOMNI(dt_start, dt_stop, data_dir=data_dir)
+    # loadObj.laod_OMNI_data() # load all the parameters
+    loadObj.load_OMNI_data(paras_in=paras_in)
+    BxGSM = loadObj.paras['BxGSM']
+    datetimelist = loadObj.paras['datetime']
