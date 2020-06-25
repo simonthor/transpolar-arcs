af142226619074ead85c8d4ce4373a53a54bbdac
Simon Thor <simon11.thor@hotmail.se>
2020-06-25 23:50:57 +0200

    Fixed bug in chi-square test.

--- plotting/analysis.py
+++ plotting/analysis.py
@@ -2,10 +2,15 @@ import numpy as np
 from scipy import stats
 
 
-def compare_dists(sample: np.ndarray, comparison: np.ndarray, dof: int = 0, priority='sample', order=0) -> float:
+def compare_dists(sample: np.ndarray, comparison: np.ndarray, bins: np.ndarray, dof: int = 0) -> float:
+    """Calculate chi-square test p-value for sample and comparison.
+    Will bin samples with fewer values than smaller than 5"""
+    if not (comparison < 5).sum():
+        _, p_value = stats.chisquare(sample, comparison, dof)
+        return p_value
 
-    if (sample < 5).sum() or (comparison < 5).sum():
-        # Merge categories to make them at least 5
-        pass
+    for bin_val, bin in zip(comparison, bins):
+        if bin_val < 5:
+            # Merge categories to make them at least 5
+            pass
 
-    return stats.chisquare(sample, comparison, dof)
