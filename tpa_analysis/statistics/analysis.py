14029cba6007a4b0197c53dba3401392d1bd9eea
Simon Thor <simon11.thor@hotmail.se>
2020-07-17 23:28:20 +0200

    Fized bug in compare_dists function. Chi2 value should now be correctly calculated.

--- tpa_analysis/statistics/analysis.py
+++ tpa_analysis/statistics/analysis.py
@@ -14,33 +14,35 @@ def compare_dists(sample: np.ndarray, comparison: np.ndarray, bins: np.ndarray, 
         _, p_value = stats.chisquare(sample, comparison, *args, **kwargs)
         return p_value
 
-    merged_comparison, merged_sample, merged_bins = np.copy(comparison), np.copy(sample), np.copy(bins)
-    for i, (sample_val, comparison_val, bin) in enumerate(zip(merged_sample[:-1], merged_comparison[:-1], merged_bins[:-1])):
-        if comparison_val < 5:
-            # Merge categories to make them at least 5
-            merged_sample[i+1] += sample_val
-            merged_comparison[i+1] += comparison_val
-            merged_sample[i] = -1
-            merged_comparison[i] = -1
-            # TODO: do something with merged_bins too
-    # Remove all invalid values and bins (in this case with value -1)
-    merged_sample = merged_sample[merged_sample != -1]
-    merged_comparison = merged_comparison[merged_comparison != -1]
+    merged_comparison, merged_sample, merged_bins = np.copy(comparison), np.copy(sample), np.array(bins, copy=True, dtype=float)
 
-    for i, (sample_val, comparison_val, bin) in reversed(list(enumerate(zip(merged_sample[1:], merged_comparison[1:], merged_bins[1:])))):
-        if comparison_val < 5:
-            # Merge categories to make them at least 5
-            merged_sample[i-1] += sample_val
-            merged_comparison[i-1] += comparison_val
-            merged_sample[i] = -1
+    for i in range(merged_comparison.size-1):
+        if merged_comparison[i] < 5:
+            merged_comparison[i+1] += merged_comparison[i]
             merged_comparison[i] = -1
-            # TODO: do something with merged_bins too
-    merged_sample = merged_sample[merged_sample != -1]
-    merged_comparison = merged_comparison[merged_comparison != -1]
+            merged_sample[i+1] += merged_sample[i]
+            merged_sample[i] = -1
+            merged_bins[i+1] = np.nan
 
-    # TODO: this often raises error, something is wrong with this function
+    merged_comparison = merged_comparison[merged_comparison != -1]
+    merged_sample = merged_sample[merged_sample != -1]
+    merged_bins = merged_bins[~np.isnan(merged_bins)]
+
+    for i in reversed(range(1, merged_comparison.size)):
+        if merged_comparison[i] < 5:
+            merged_comparison[i-1] += merged_comparison[i]
+            merged_comparison[i] = -1
+            merged_sample[i-1] += merged_sample[i]
+            merged_sample[i] = -1
+            merged_bins[i] = np.nan
+
+    merged_comparison = merged_comparison[merged_comparison != -1]
+    merged_sample = merged_sample[merged_sample != -1]
+    merged_bins = merged_bins[~np.isnan(merged_bins)]
+
     assert (merged_comparison >= 5).all(), merged_comparison
 
-    print(merged_sample, merged_comparison)
+    print(f'{merged_sample=}\n{merged_comparison=}\n{merged_bins=}')
+
     _, p_value = stats.chisquare(merged_sample, merged_comparison, *args, **kwargs)
     return p_value
