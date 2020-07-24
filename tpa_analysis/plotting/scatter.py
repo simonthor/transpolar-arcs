acc2320404425586d5b6aff03948503d64280cdd
Simon Thor <simon11.thor@hotmail.se>
2020-07-24 21:36:35 +0200

    Fixed bugs in scatter_template. Created usable version

--- tpa_analysis/plotting/scatter.py
+++ tpa_analysis/plotting/scatter.py
@@ -3,29 +3,34 @@ from contextlib import contextmanager
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
+import matplotlib
 
 
 # TODO: create template figure for scatter plots? E.g:
 @contextmanager
-def scatter_template(xlim=None, ylim=None, save=False, *args, **kwargs):
+def scatter_template(save=False, *args, **kwargs):
     fig, ax = plt.subplots(*args, **kwargs)
-    ax.axhline(0, color='grey', zorder=-1)
-    ax.axvline(0, color='grey', zorder=-1)
+    if isinstance(ax, Axes):
+        ax.axhline(0, color='grey', zorder=-1)
+        ax.axvline(0, color='grey', zorder=-1)
+    else:
+        for a in ax.flat:
+            a.axhline(0, color='grey', zorder=-1)
+            a.axvline(0, color='grey', zorder=-1)
+
     yield fig, ax
-    ax.set_xlim(xlim)
-    ax.set_ylim(ylim)
+
+    fig.tight_layout()
     if save:
         fig.savefig(save)
-    fig.show()
-    fig.close()
+    if 'inline' not in matplotlib.get_backend():
+        fig.show()
+    # TODO: Add functionality for deleting figure after use? E.g.: fig.clf()
 
 
 def scatter(x, y, axis: Axes, dataset_name: str, marker_color: str = 'k', *args, **kwargs):
     axis.axhline(0, color='grey', zorder=-1)
     axis.axvline(0, color='grey', zorder=-1)
-    omit_index = np.isnan(x) | np.isnan(y)
-    x = x[~omit_index]
-    y = y[~omit_index]
     axis.scatter(x, y, s=30, marker='P', edgecolors='w', linewidth=0.5, label=dataset_name, c=marker_color, zorder=2,
                  *args, **kwargs)
     axis.legend(loc=1)
