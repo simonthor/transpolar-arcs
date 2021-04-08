4c445791b3c25d4431a9b5555ba89fb81ad569a5
Simon Thor <simon11.thor@hotmail.se>
2021-04-08 15:12:59 +0200

    Fix bug when single subplot was created. Add subplot_labels

--- tpa_analysis/plotting/scatter.py
+++ tpa_analysis/plotting/scatter.py
@@ -1,20 +1,20 @@
 from contextlib import contextmanager
-
 from matplotlib import pyplot as plt
-from matplotlib.axes import Axes
 import matplotlib
+import numpy as np
 
 
-# TODO: create template figure for scatter plots? E.g:
 @contextmanager
 def scatter_template(save: str = False, *args, **kwargs):
     fig, ax = plt.subplots(*args, **kwargs)
-    if isinstance(ax, Axes):
-        ax = [ax]
+    if not isinstance(ax, np.ndarray):
+        ax = np.array([ax])
 
-    for a in ax.flat:
+    for a, letter in zip(ax.flat, 'abcdefghijklmnopqrstuvwxyz'.upper()):
         a.axhline(0, color='grey', zorder=-1)
         a.axvline(0, color='grey', zorder=-1)
+        from . import subplot_label
+        subplot_label(a, letter)
 
     yield fig, ax
 
@@ -26,9 +26,14 @@ def scatter_template(save: str = False, *args, **kwargs):
     # TODO: Add functionality for deleting figure after use? E.g.: fig.clf()
 
 
-def scatter(x, y, axis: Axes, dataset_name: str, marker_color: str = 'k', *args, **kwargs):
+def scatter(x, y, axis, dataset_name: str, marker_color: str = 'k', *args, **kwargs):
     axis.axhline(0, color='grey', zorder=-1)
     axis.axvline(0, color='grey', zorder=-1)
     axis.scatter(x, y, s=30, marker='P', edgecolors='w', linewidth=0.5, label=dataset_name, c=marker_color, zorder=2,
                  *args, **kwargs)
     axis.legend(loc=1)
+
+
+if __name__ == '__main__':
+    with scatter_template() as (fig, ax):
+        ax[0].plot([0, 1], [0, 1])
